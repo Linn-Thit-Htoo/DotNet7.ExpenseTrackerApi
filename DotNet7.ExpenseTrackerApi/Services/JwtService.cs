f43889c3d3a3c8b5e8beaeb68daf2f1e6e7e2c17
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:48:16 +0630

    -

--- DotNet7.ExpenseTrackerApi/Services/JwtService.cs
+++ DotNet7.ExpenseTrackerApi/Services/JwtService.cs
@@ -1,5 +1,4 @@
 ﻿using DotNet7.ExpenseTrackerApi.Models.Entities;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
