f00d3305b847454801630c1d17958fe9785a8fc1
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:48:48 +0630

    -

--- DotNet7.ExpenseTrackerApi/AppDbContext.cs
+++ DotNet7.ExpenseTrackerApi/AppDbContext.cs
@@ -1,19 +1,18 @@
 ﻿using DotNet7.ExpenseTrackerApi.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNet7.ExpenseTrackerApi
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions options) : base(options)
-        {
-        }
+namespace DotNet7.ExpenseTrackerApi;
 
-        public DbSet<IncomeModel> Income { get; set; }
-        public DbSet<IncomeCategoryModel> IncomeCategory { get; set; }
-        public DbSet<ExpenseModel> Expense { get; set; }
-        public DbSet<ExpenseCategoryModel> Expense_Category { get; set; }
-        public DbSet<BalanceModel> Balance { get; set; }
-        public DbSet<UserModel> Users { get; set; }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
     }
+
+    public DbSet<IncomeModel> Income { get; set; }
+    public DbSet<IncomeCategoryModel> IncomeCategory { get; set; }
+    public DbSet<ExpenseModel> Expense { get; set; }
+    public DbSet<ExpenseCategoryModel> Expense_Category { get; set; }
+    public DbSet<BalanceModel> Balance { get; set; }
+    public DbSet<UserModel> Users { get; set; }
 }
