0b4698aaa4b63d3d785c63a3541a3b3dccec75ef
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:49:49 +0630

    -

--- DotNet7.ExpenseTrackerApi/Services/ModularService.cs
+++ DotNet7.ExpenseTrackerApi/Services/ModularService.cs
@@ -32,6 +32,7 @@ public static class ModularService
 
         return services;
     }
+
     #endregion
 
     public static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
