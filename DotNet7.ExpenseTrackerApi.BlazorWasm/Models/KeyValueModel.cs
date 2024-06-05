7dbb9e3068e262eafee3caac69014f1622ae11f8
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-05 22:02:14 +0630

    -

--- DotNet7.ExpenseTrackerApi.BlazorWasm/Models/KeyValueModel.cs
+++ DotNet7.ExpenseTrackerApi.BlazorWasm/Models/KeyValueModel.cs
@@ -1,14 +1,13 @@
-﻿namespace DotNet7.ExpenseTrackerApi.BlazorWasm.Models
-{
-    public class KeyValueModel
-    {
-        public string Key { get; set; }
-        public string Value { get; set; }
+﻿namespace DotNet7.ExpenseTrackerApi.BlazorWasm.Models;
 
-        public KeyValueModel(string key, string value)
-        {
-            Key = key;
-            Value = value;
-        }
+public class KeyValueModel
+{
+    public string Key { get; set; }
+    public string Value { get; set; }
+
+    public KeyValueModel(string key, string value)
+    {
+        Key = key;
+        Value = value;
     }
 }
