b59d4e6953d50d9bad1903dfaac3a7f4843c3b36
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-05 22:01:16 +0630

    -

--- DotNet7.ExpenseTrackerApi.BlazorWasm/Pages/Authentication/P_Register.razor.cs
+++ DotNet7.ExpenseTrackerApi.BlazorWasm/Pages/Authentication/P_Register.razor.cs
@@ -1,29 +1,28 @@
 ﻿using DotNet7.ExpenseTrackerApi.BlazorWasm.Models.User;
 using System.Text.RegularExpressions;
 
-namespace DotNet7.ExpenseTrackerApi.BlazorWasm.Pages.Authentication
+namespace DotNet7.ExpenseTrackerApi.BlazorWasm.Pages.Authentication;
+
+public partial class P_Register
 {
-    public partial class P_Register
+    RegisterRequestModel requestModel = new();
+    private bool isBtnDisabled = true;
+
+    private void Validate()
     {
-        RegisterRequestModel requestModel = new();
-        private bool isBtnDisabled = true;
+        isBtnDisabled = IsNullOrEmpty(requestModel.UserName) || IsNullOrEmpty(requestModel.Email)
+            || !IsValidEmail(requestModel.Email)
+            || IsNullOrEmpty(requestModel.Password) || requestModel.DOB == default
+            || IsNullOrEmpty(requestModel.Gender);
+    }
 
-        private void Validate()
-        {
-            isBtnDisabled = IsNullOrEmpty(requestModel.UserName) || IsNullOrEmpty(requestModel.Email)
-                || !IsValidEmail(requestModel.Email)
-                || IsNullOrEmpty(requestModel.Password) || requestModel.DOB == default
-                || IsNullOrEmpty(requestModel.Gender);
-        }
+    private bool IsNullOrEmpty(string str)
+    {
+        return string.IsNullOrEmpty(str);
+    }
 
-        private bool IsNullOrEmpty(string str)
-        {
-            return string.IsNullOrEmpty(str);
-        }
-
-        private bool IsValidEmail(string str)
-        {
-            return Regex.IsMatch(str, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
-        }
+    private bool IsValidEmail(string str)
+    {
+        return Regex.IsMatch(str, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
     }
 }
