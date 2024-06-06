d1833057099581f05030d4436f88fa8ef40dff1f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-06-06 16:42:15 +0630

    Update Datetime default

--- DotNet7.ExpenseTrackerApi.BlazorWasm/Models/User/RegisterRequestModel.cs
+++ DotNet7.ExpenseTrackerApi.BlazorWasm/Models/User/RegisterRequestModel.cs
@@ -10,6 +10,6 @@ public class RegisterRequestModel
     [MaxLength(12)]
     [MinLength(6)]
     public string Password { get; set; } = null!;
-    public DateTime? DOB { get; set; }
+    public DateTime? DOB { get; set; } == default;
     public string Gender { get; set; } = null!;
 }
