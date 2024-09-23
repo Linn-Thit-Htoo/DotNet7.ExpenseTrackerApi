e03c945968ac6265d061d31e75996c26424f0b44
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-23 09:55:55 +0630

    -

--- DotNet7.ExpenseTrackerApi.Shared/HashPasswordService.cs
+++ DotNet7.ExpenseTrackerApi.Shared/HashPasswordService.cs
@@ -1,35 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
-using XSystem.Security.Cryptography;
 
 namespace DotNet7.ExpenseTrackerApi.Shared
 {
     public class HashPasswordService
     {
-        public string HashPassword(string password, byte[] salt)
-        {
-            using (var sha256 = new SHA256Managed())
-            {
-                byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-                byte[] saltedPassword = new byte[passwordBytes.Length + salt.Length];
-
-                // Concatenate password and salt
-                Buffer.BlockCopy(passwordBytes, 0, saltedPassword, 0, passwordBytes.Length);
-                Buffer.BlockCopy(salt, 0, saltedPassword, passwordBytes.Length, salt.Length);
-
-                // Hash the concatenated password and salt
-                byte[] hashedBytes = sha256.ComputeHash(saltedPassword);
-
-                // Concatenate the salt and hashed password for storage
-                byte[] hashedPasswordWithSalt = new byte[hashedBytes.Length + salt.Length];
-                Buffer.BlockCopy(salt, 0, hashedPasswordWithSalt, 0, salt.Length);
-                Buffer.BlockCopy(hashedBytes, 0, hashedPasswordWithSalt, salt.Length, hashedBytes.Length);
-
-                return Convert.ToBase64String(hashedPasswordWithSalt);
-            }
-        }
     }
 }
